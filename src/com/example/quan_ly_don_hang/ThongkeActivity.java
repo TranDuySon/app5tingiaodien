94dab7f6925695f6d469cecdf8e94cf5f457dd43
duysonidol <sonidol96@gmail.com>
2018-03-14 01:12:44 +0700

    Fix thống kê

--- src/com/example/quan_ly_don_hang/ThongkeActivity.java
+++ src/com/example/quan_ly_don_hang/ThongkeActivity.java
@@ -59,6 +59,7 @@ public class ThongkeActivity extends Activity {
 		});
 	}
 	public void display(){
+		arrayAdapter.clear();
 cursor = db.Thongke(edtngay.getText().toString().trim());
 while (cursor.moveToNext()){
 	arrayAdapter.add(new Thongke(cursor.getString(0), cursor.getInt(1), cursor.getInt(2)));
